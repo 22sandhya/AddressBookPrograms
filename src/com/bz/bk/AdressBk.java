181df7c8ea66d32aed3ba5ba398eb55bf84b637f
Admin <sandhyabagale10@gmail.com>
2021-10-09 14:27:05 +0530

    Added:person name is deleted

--- src/com/bz/bk/AdressBk.java
+++ src/com/bz/bk/AdressBk.java
@@ -7,15 +7,22 @@ public class AdressBk {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Address Book System code !!!");
 		Scanner sc = new Scanner(System.in);
-		AddressBookDe person = new AddressBookDe();
-		person.addContact();// Calling Add Contacts Method
-		System.out.println("Enter Y To Edit The Contact");
-		String op = sc.nextLine();
-
-		if (op.equals("y") || op.equals("Y")) {
-
+		AddressBookDe Book = new AddressBookDe();
+		Book.addContact();// Calling Add Contacts Method
+		System.out.println(Book.person);
+		System.out.println("Enter the choice \n 1. Edit \n 2. Delete");
+		int option = sc.nextInt();
+		switch (option) {
+		case 1:
+			Book.editContact();
 			System.out.println("You have Entered following data");
-			System.out.println("The Contact Details After Editing : " + person);
+			System.out.println(Book.person);
+			System.out.println("Thank you for Using the Address book");
+			break;
+		case 2:
+			Book.deleteContact();
+			System.out.println("Address Book details :" + Book.person);
+			break;
 		}
 	}
 }
@@ -94,11 +101,12 @@ class contactDetails {
 }
 
 class AddressBookDe {
-
 	Scanner sc = new Scanner(System.in);
+
 	contactDetails person = new contactDetails();
 
 	public void addContact() {
+		// contactDetails person = new contactDetails();
 		System.out.println("Enter First Name: ");
 		String firstName = sc.nextLine();
 		System.out.println("Enter last Name: ");
@@ -126,6 +134,7 @@ class AddressBookDe {
 	}
 
 	public void editContact() {
+		// Scanner editName = new Scanner(System.in);
 		System.out.println("Enter the firstName of person");
 		String editName = sc.nextLine();
 		if (editName.equalsIgnoreCase(person.getFirstName()))
@@ -133,5 +142,14 @@ class AddressBookDe {
 		else
 			System.out.println("The Entered First Name Is Not Match");
 		editContact();
+	}
+
+	public void deleteContact() {
+		System.out.println("Enter firstName of the person");
+		String editName = sc.nextLine();
+		if (editName.equals(person.getFirstName())) {
+			System.out.println("Deleted " + person.getFirstName() + " details");
+			person = null;
+		}
 	}
 }
